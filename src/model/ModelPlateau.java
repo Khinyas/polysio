f9adc784c87463aa313ccb5acc8e22b0560d6870
Lucas Alves Rosa <lucasar29000@gmail.com>
2026-03-05 08:58:49 +0100

    model plateau

---
+++ src/model/ModelPlateau.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.ArrayList;
+
+public class ModelPlateau {
+    private ArrayList<ModelCase> listeCases = new ArrayList<>();
+
+    public ModelPlateau() {
+        for (int i = 0; i < 40; i++) {
+            listeCases.add(new ModelCase(i, "Rue " + i, "Bleu", i * 10));
+        }
+    }
+
+    public ArrayList<ModelCase> getListeCases() {
+        return listeCases;
+    }
+}
